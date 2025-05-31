aaffb0f661a249c235f5c1fd87783bf3f5f60bd2
phanhuy250205 <162803292+phanhuy250205@users.noreply.github.com>
2025-05-31 05:18:56 -0500

    Nhà trọ xanh

---
+++ Nhatroxanh/src/main/java/nhatroxanh/com/Nhatroxanh/NhatroxanhApplication.java
@@ -0,0 +1,13 @@
+package nhatroxanh.com.Nhatroxanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhatroxanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhatroxanhApplication.class, args);
+	}
+
+}
---
+++ Nhatroxanh/src/test/java/nhatroxanh/com/Nhatroxanh/NhatroxanhApplicationTests.java
@@ -0,0 +1,13 @@
+package nhatroxanh.com.Nhatroxanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhatroxanhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
