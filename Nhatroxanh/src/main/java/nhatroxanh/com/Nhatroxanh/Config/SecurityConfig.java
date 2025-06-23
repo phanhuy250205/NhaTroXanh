14662a5fc3f0d267f54c6dc37b1626be7ef2279f
Hoangloi <hoangloi@gmail.com>
2025-06-23 12:05:46 +0700

    Uodate link staff

--- Nhatroxanh/src/main/java/nhatroxanh/com/Nhatroxanh/Config/SecurityConfig.java
+++ Nhatroxanh/src/main/java/nhatroxanh/com/Nhatroxanh/Config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
                         .requestMatchers("/", "/trang-chu", "/css/**", "/js/**", "/images/**", "/bootstrap/**",
                                 "/fonts/**", "/uploads/**")
                         .permitAll()
-                        .requestMatchers("/phong-tro/**", "/chi-tiet/**", "/danh-muc/**","/chu-tro/**").permitAll()
+                        .requestMatchers("/phong-tro/**", "/chi-tiet/**", "/danh-muc/**","/chu-tro/**", "/nhan-vien/**").permitAll()
                         // Mọi request khác cần xác thực
                         .anyRequest().authenticated())
                 .formLogin(form -> form
